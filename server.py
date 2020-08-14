e1b94c59014eba729886c1af86e135200ad7b58a
Norris Mei <norrismei3@gmail.com>
2020-08-14 14:12:23 -0700

    Created skeleton of Flask app routes and html templates

--- server.py
+++ server.py
@@ -11,6 +11,29 @@ app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
+
+@app.route('/users/<username>')
+def show_user_homepage(username):
+    """View homepage"""
+
+    #If not logged in, redirect to login page
+
+    return render_template('user_page.html')
+
+
+@app.route('/games')
+def show_all_games():
+    """View search to add games to personal list"""
+
+    return render_template('all_games.html')
+
+
+@app.route('/marketplace')
+def show_game_database():
+    """View list of games for sale"""
+
+    return render_template('marketplace.html')
+
 if __name__ == '__main__':
     connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
